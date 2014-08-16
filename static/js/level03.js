c4a7a5753e854fd59a04f26ea7bab3e93c8a2e5e
amerizalde <andrewmerizalde@hotmail.com>
2014-08-16 01:02:15 -0600

    added sun, normal mapping material to globe

--- static/js/level03.js
+++ static/js/level03.js
@@ -9,10 +9,14 @@ EarthApp.prototype = new Sim.App();
 EarthApp.prototype.init = function (param) {
     // super().__init__()
     Sim.App.prototype.init.call(this, param);
-    
+
     var earth = new Earth();
     earth.init();
     this.addObject(earth);
+
+    var sun = new Sun();
+    sun.init();
+    this.addObject(sun);
 };
 
 var Earth = function () {
@@ -21,18 +25,51 @@ var Earth = function () {
 
 Earth.prototype = new Sim.Object();
 Earth.prototype.init = function () {
-    // Create an Earth sphere with texture
-    var earthMap = "assets/images/earth_surface_2048.jpg",
-        geom = new THREE.SphereGeometry(1, 32, 32),
-        texture = THREE.ImageUtils.loadTexture(earthMap),
-        material = new THREE.MeshBasicMaterial( {map: texture}),
-        mesh = new THREE.Mesh( geom, material);
-    
-    mesh.rotation.z = Earth.TILT;
-    
-    // tell framework about the object
-    this.setObject3D(mesh);
+    // create a group
+    var earthGroup = new THREE.Object3D();
+    this.setObject3D(earthGroup);
+
+    this.createGlobe();
+    this.createClouds();
 };
+
+Earth.prototype.createGlobe = function () {
+    // the maps
+    var diffuseMap = THREE.ImageUtils.loadTexture('assets/images/earth_surface_2048.jpg'),
+        normalMap = THREE.ImageUtils.loadTexture('assets/images/earth_normal_2048.jpg'),
+        specularMap = THREE.ImageUtils.loadTexture('assets/images/earth_specular_2048.jpg'),
+        shader = THREE.ShaderLib["normalmap"];
+
+    shader.uniforms["tDiffuse"].value = diffuseMap;
+    shader.uniforms["tNormal"].value = normalMap;
+    shader.uniforms["tSpecular"].value = specularMap;
+    shader.uniforms["enableDiffuse"].value = 1; // on
+    shader.uniforms["enableSpecular"].value = 1; // on
+
+    console.log(shader.uniforms);
+
+    // Create an Earth sphere with texture
+    var geom = new THREE.SphereGeometry(1, 32, 32);
+    geom.computeTangents();
+
+    var material = new THREE.ShaderMaterial({
+        fragmentShader: shader.fragmentShader,
+        vertexShader: shader.vertexShader,
+        uniforms: shader.uniforms,
+        lights: true
+    }),
+        mesh = new THREE.Mesh( geom, material);
+
+    mesh.rotation.z = Earth.TILT;
+
+    // tell framework about the object
+    this.object3D.add(mesh);
+    this.globeMesh = mesh;
+}
+
+Earth.prototype.createClouds = function () {
+
+}
 
 Earth.prototype.update = function () {
     this.object3D.rotation.y += Earth.ROTATION_Y;
@@ -40,6 +77,19 @@ Earth.prototype.update = function () {
 
 Earth.ROTATION_Y = 0.0025;
 Earth.TILT = 0.41;
+
+// sun light
+var Sun = function () {
+    Sim.Object.call(this);
+}
+
+Sun.prototype = new Sim.Object();
+
+Sun.prototype.init = function () {
+    var light = new THREE.PointLight( 0xffffff, 2, 100);
+    light.position.set(-10, 0, 20);
+    this.setObject3D(light);
+}
 
 $(document).ready( function () {
     var app = new EarthApp();
